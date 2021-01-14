6e886e13fccb47d76f47c851061e47f0c8069775
home <dadasay41@gmail.com>
2021-01-14 22:26:33 +0600

    Show Work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    private void isRain(MouseEvent event)
+    {
+        System.out.println("Test");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1220, 600));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void nothing(Checkbox checkbox){
+        System.out.println(1);
     }
 }
